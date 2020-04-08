fe906383e24a40169b97bf9fe408199ee3ca8ba7
Егор <35236837+Far-East@users.noreply.github.com>
2020-04-08 11:22:39 +0300

    Меняю местами вкладки и апсейлы на странице продуктов

--- tabs-upsells.php
+++ tabs-upsells.php
@@ -5,5 +5,5 @@ add_action( 'woocommerce_after_single_product_summary', 'bbloomer_woocommerce_ou
 function bbloomer_woocommerce_output_upsells() {
 	woocommerce_upsell_display();
 	// Display max 3 products, 3 per row
-	// woocommerce_upsell_display( 3,3 );
+	// woocommerce_upsell_display( 3,3 );  
 }
